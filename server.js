96e4185a27ff4ec2ca81d1b6a9a8e38140005153
Dmitriy Soldatov <dmitrsold@gmail.com>
2019-11-06 01:11:18 +0200

    just test only prod mode

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const options = {
 
 const bot = new TelegramBot(TOKEN);
 
-process.env.NODE_ENV === 'production' ? startProdMode(bot) : startDevMode(bot);
+process.env.NODE_ENV === 'production' ? startProdMode(bot) : process.exit(1);
 
 async function startDevMode(bot) {
   try {
